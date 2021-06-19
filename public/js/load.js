6a3a5600e2db6442b946e9356ecf2af1dc94396b
Spencer Sablan <spencertsablan@gmail.com>
2021-06-18 17:10:49 -0700

    Added hbs, templates structure, & created loader

---
+++ public/js/load.js
@@ -0,0 +1,3 @@
+$(window).load(() => {
+    $('.loading').fadeOut()
+})
--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 require('./db/mongoose')
 const userRouter = require('./routes/user-router')
 const friendRouter = require('./routes/friend-router')
@@ -8,6 +10,27 @@ app.use(express.json())
 app.use(userRouter)
 app.use(friendRouter)
 
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine & views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to server
+app.use(express.static(publicDirectoryPath))
+
+app.get('/', (req,res) => {
+    res.render('index', {
+        title: 'WORKING!'
+    })
+})
+
 module.exports = app
+
 
 // TODO: Once we build a frontend, build out scheduling with agenda for user.
